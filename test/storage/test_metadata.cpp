a085cf2403e4eaf94ef15a17590acd87ed9d6e98
hexu <hexu_1985@sina.com>
2018-06-03 08:36:38 +0800

    mv print from test_metadata.cpp to print_metadata.cpp

--- test/storage/test_metadata.cpp
+++ test/storage/test_metadata.cpp
@@ -1,39 +1,12 @@
 #include "datafile_metadata.h"
+#include "print_metadata.h"
 #include "dump_data.h"
 #include <string>
 #include <chrono>
-#include <map>
 #include <iostream>
 
 using namespace std;
 using namespace std::chrono;
-
-string utc_to_string(const system_clock::time_point &tp)
-{
-    char buffer[80];
-    time_t tt = system_clock::to_time_t(tp);
-    struct tm timeinfo;
-
-    {
-        struct tm *ptm = gmtime(&tt);
-        timeinfo = *ptm;
-    }
-
-    strftime (buffer,80,"%Y-%m-%d,%H:%M:%S",&timeinfo);
-
-    return string(buffer);
-}
-
-void print(const DatafileMetadata &meta, const string &title)
-{
-    cout << title << "\n";
-    cout << "\tmeta.etag: " << meta.etag << "\n";
-    cout << "\tmeta.content_type: " << meta.content_type << "\n";
-    cout << "\tmeta.creation_time: " << utc_to_string(meta.creation_time) << "\n";
-
-    for (auto &attr: meta.attrs)
-        cout << "\tmeta.attrs[" << attr.first << "]: " << attr.second << "\n";
-}
 
 int main(int argc, char *argv[])
 {
