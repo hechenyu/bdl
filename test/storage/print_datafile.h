af6cdb020de5069bb8bd216f9a4be4b6267952cd
hexu <hexu_1985@sina.com>
2018-06-03 15:28:07 +0800

    complete test datafile_view

---
+++ test/storage/print_datafile.h
@@ -0,0 +1,10 @@
+#ifndef __print_datafile_h
+#define __print_datafile_h
+
+#include "datafile_view.h"
+#include <string>
+
+void print(const DatafileView &view, const std::string &title);
+
+#endif
+
--- test/storage/test_datafile.cpp
+++ test/storage/test_datafile.cpp
@@ -1,7 +1,9 @@
 #include "datafile_config.h"
 #include "datafile_metadata.h"
 #include "datafile_serializer.h"
+#include "datafile_view.h"
 #include "print_metadata.h"
+#include "print_datafile.h"
 #include "dump_data.h"
 #include <string>
 #include <chrono>
@@ -28,10 +30,10 @@ int main(int argc, char *argv[])
     vector<uint8_t> serialize_data = datafile_serializer.serialize(blob.data(), blob.size(), meta);
     assert(serialize_data.size() % DatafileConfig::kAlignSize == 0);
 
-    dump_data("\nmeta serialize", (const char *) serialize_data.data(), serialize_data.size());
+    dump_data("\ndatafile serialize", (const char *) serialize_data.data(), serialize_data.size());
 
-//    DatafileMetadata meta2(serialize_data);
-//    print(meta2, "metadata from serialize_data:");
+    DatafileView datafile_view(serialize_data.data(), serialize_data.size());
+    print(datafile_view, "datafile from serialize_data:");
 
     return 0;
 }
