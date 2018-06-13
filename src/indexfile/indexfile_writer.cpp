0fbe8af0aa15682afc704f75ed8520877efce0aa
hexu <hexu_1985@sina.com>
2018-06-14 01:14:56 +0800

    add indexfile item valid check

--- src/indexfile/indexfile_writer.cpp
+++ src/indexfile/indexfile_writer.cpp
@@ -1,3 +1,4 @@
+#include "error.h"
 #include "indexfile_writer.h"
 
 using namespace std;
@@ -14,7 +15,14 @@ IndexfileWriter::~IndexfileWriter()
 
 void IndexfileWriter::write(const IndexfileItem &item)
 {
+    check_item_valid(item);
+
     auto data = item.serialize();
     writer_->putline(data);
 }
 
+void IndexfileWriter::check_item_valid(const IndexfileItem &item)
+{
+    if (item.key.find_first_of(",") != string::npos)
+        err_quit("Invalid indexfile item: %s", item.key.c_str());
+}
