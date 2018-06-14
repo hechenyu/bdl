5b03ff5f1aefb426769d11ecbffac18aa48632cd
hexu <hexu_1985@sina.com>
2018-06-15 00:13:35 +0800

    complete dataset_read

--- test/dataset/dataset_read.cpp
+++ test/dataset/dataset_read.cpp
@@ -1,24 +1,64 @@
 #include <memory>
 #include <iostream>
+#include "boost/filesystem.hpp"
+#include "config_parser.h"
 #include "posix_file_system.h"
 #include "io_context.h"
 #include "dataset_index.h"
 #include "print_dataset_index_item.h"
 
 using namespace std;
+namespace fs = boost::filesystem;
+
+void write_file(const string &datafile_name, const string &data)
+{
+    ofstream ofile(datafile_name);
+    ofile.write(data.data(), data.size());
+}
 
 int main(int argc, char *argv[])
 {
-    string root = "/tmp";
-    string index_path = "file_set";
+    ConfigParser parser(argv[0]);
+    parser.add_option("help,h")
+          .add_option("list,t", "list datafile in partitions")
+          .add_string_option("root", "root of io_context")
+          .add_string_option("dataset", "dataset index name")
+          .add_string_option("dir,d", "output directory(default .)");
 
-    auto io_context = make_shared<IOContext>(root, make_shared<PosixFileSystem>());
-    DatasetIndex index(io_context, index_path, "r");
+    parser.parse_command_line(argc, argv);
+
+    if (parser.has_parsed_option("help")) {
+        parser.print_options_description(cout);
+        exit(1);
+    }
+
+    string root_name = parser.get_string_variables("root", "/tmp");
+    string dataset_index_name = parser.get_string_variables("dataset", "file_set");
+    string output_dir = parser.get_string_variables("dir", "");
+    bool list_flag = parser.has_parsed_option("list");
+
+    auto io_context = make_shared<IOContext>(root_name, make_shared<PosixFileSystem>());
+    DatasetIndex index(io_context, dataset_index_name, "r");
+
+    if (!output_dir.empty())
+        fs::create_directories(output_dir);
 
     auto index_files = index.getIndexFiles();
     for (auto index_file: index_files) {
         for (auto item: index_file) {
-            cout << item << "\n";
+            if (list_flag)
+                cout << item << "\n";
+            
+            auto datafile_name = item.file_path();
+            auto datafile = index.openFile(item);
+            auto data = datafile.readAll();
+
+            if (!output_dir.empty())
+                datafile_name = output_dir+"/"+datafile_name;
+            fs::path path(datafile_name);
+
+            fs::create_directories(path.parent_path());
+            write_file(datafile_name, data);
         }
     }
 
