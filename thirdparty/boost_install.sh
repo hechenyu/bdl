972dea7df47c9221d36bdecafd2d0f64b1053880
hexu <hexu@sensetime.com>
2018-06-21 15:10:30 +0800

    update boost_install.sh

--- thirdparty/boost_install.sh
+++ thirdparty/boost_install.sh
@@ -5,9 +5,9 @@ cmd_path=$(dirname $0)
 echo $cmd_path
 cd $cmd_path
 
-tar xzvf boost_1_67_0.tar.gz || echo "tar boost_1_67_0.tar.gz fail"; exit 1
+#wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz
+tar xzvf boost_1_67_0.tar.gz
 cd boost_1_67_0
 
-#wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz
 ./bootstrap.sh --with-toolset=gcc link=static runtime-link=static threading=multi stage --with-python=python3
 ./b2 -j4 cflags=-fPIC cxxflags=-fPIC
