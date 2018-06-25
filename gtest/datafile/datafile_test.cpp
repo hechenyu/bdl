ca9a183c9ce5c6dfa902c3f29b60134b0714547b
hexu <hexu_1985@sina.com>
2018-06-26 01:49:25 +0800

    add ut for datafile_test.cpp

--- gtest/datafile/datafile_test.cpp
+++ gtest/datafile/datafile_test.cpp
@@ -1,4 +1,7 @@
+#include "datafile_config.h"
 #include "datafile_metadata.h"
+#include "datafile_serializer.h"
+#include "datafile_view.h"
 #include <string>
 #include <chrono>
 #include <iostream>
@@ -29,3 +32,17 @@ TEST(DatafileTest, Metadata)
     EXPECT_EQ(meta.file_name(), meta2.file_name());
 }
 
+TEST(DatafileTest, Datafile)
+{
+    string blob = "hello world!";
+
+    DatafileMetadata meta("test.txt", "plain/txt", (const uint8_t *) blob.data(), blob.size(), {{"key1", "value1"}, {"key2", "value2"}});
+
+    DatafileSerializer datafile_serializer;
+    vector<uint8_t> serialize_data = datafile_serializer.serialize((const uint8_t *) blob.data(), blob.size(), meta);
+    ASSERT_TRUE(serialize_data.size() % DatafileConfig::kAlignSize == 0);
+
+    DatafileView datafile_view(serialize_data.data(), serialize_data.size());
+    string blob2((const char *)datafile_view.blob_data(), datafile_view.blob_size());
+    EXPECT_EQ(blob, blob2);
+}
