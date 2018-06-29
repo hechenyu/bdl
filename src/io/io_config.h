5a55d23137e640b5cabb51b8d8d24eca79a3ed02
hexu <hexu_1985@sina.com>
2018-06-29 22:31:27 +0800

    add io_config.h

---
+++ src/io/io_config.h
@@ -0,0 +1,10 @@
+#ifndef __io_config_h
+#define __io_config_h
+
+#include <cstdio>
+
+#ifndef STDC_FILE_READER_BUFFER_SIZE
+#define STDC_FILE_READER_BUFFER_SIZE BUFSIZ
+#endif
+
+#endif
--- test/main/main.cpp
+++ test/main/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "error.h"
+#include "io_config.h"
 #include "dataset_config.h"
 
 using namespace std;
@@ -8,6 +9,7 @@ int main(int argc, char *argv[])
 {
     err_msg("hello world!");
     cout << "dataset_config: " << DatasetConfig::printable_info() << endl;
+    std::cout << "STDC_FILE_READER_BUFFER_SIZE: " << STDC_FILE_READER_BUFFER_SIZE << std::endl;
 
     return 0;
 }
