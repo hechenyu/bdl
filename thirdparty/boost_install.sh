ffafe3124f402b851f0eb7cdd803b53bdb1696d4
hexu <hexu@sensetime.com>
2018-07-03 10:47:34 +0800

    update CMakeLists.txt

--- thirdparty/boost_install.sh
+++ thirdparty/boost_install.sh
@@ -5,9 +5,9 @@ cmd_path=$(dirname $0)
 echo $cmd_path
 cd $cmd_path
 
-#wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz
-tar xzvf boost_1_67_0.tar.gz
+tar xzvf boost_1_67_0.tar.gz 
 cd boost_1_67_0
 
-./bootstrap.sh --with-toolset=gcc --with-libraries=all --with-python=python3
-./b2 -j4 toolset=gcc cflags=-fPIC cxxflags=-fPIC link=static runtime-link=static threading=multi stage
+#wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz
+./bootstrap.sh --with-toolset=gcc --with-libraries=all --with-python=python3 --prefix=..
+./b2 -j4 toolset=gcc cflags=-fPIC cxxflags=-fPIC link=static runtime-link=static threading=multi stage install
