2fcbf7bc8d349b0a630cb553b25898f0a85d87b6
hexu <hexu@sensetime.com>
2018-07-06 14:10:16 +0800

    add conf and auto test sh

---
+++ perf_test/dataset/samp_conf/dataset.samp.seq.hdd.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+if (( $# != 2 ))
+then
+    echo "usage: $(basename $0) proc_num time_wait"
+    exit 1
+fi
+
+proc_num=$1
+time_wait=$2
+node_name=$(uname -n | cut -d. -f1)
+
+cmd=/mnt/lustressd/hx/base_dataset_loader2/perf_test/build/dataset/dataset_read_samp
+seed=1530106820
+root=/mnt/lustrehdd/hx/dataset${proc_num}
+dir=/mnt/lustressd/hx/samp/seq.hdd/${proc_num}/${node_name}
+
+i=1
+while (( i <= proc_num ))
+do
+   label=dataset.samp.seq.hdd.${node_name}.${i}
+   dataset=data${i}
+   echo $i
+   $cmd --seed ${seed} --label ${label} --root ${root} --dataset ${dataset} --dir ${dir} &
+   (( i = i + 1 ))
+done
+
+sleep $time_wait
+killall -9 dataset_read_samp
+sleep 1
+killall -9 dataset_read_samp
+#wait
+
---
+++ perf_test/dataset/samp_conf/standard.samp.hdd.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+if (( $# != 2 ))
+then
+    echo "usage: $(basename $0) proc_num time_wait"
+    exit 1
+fi
+
+proc_num=$1
+time_wait=$2
+node_name=$(uname -n | cut -d. -f1)
+
+cmd=/mnt/lustressd/hx/base_dataset_loader2/perf_test/build/dataset/standard_read_samp
+seed=-1
+list_file=/mnt/lustrehdd/hx/filelist${proc_num}/file.list
+dir=/mnt/lustressd/hx/samp/std.hdd/${proc_num}/${node_name}
+
+i=1
+while (( i <= proc_num ))
+do
+   label=standard.samp.hdd.${node_name}.${i}
+   echo $i
+   $cmd --seed ${seed} --label ${label} --list_file ${list_file}${i} --dir ${dir} &
+   (( i = i + 1 ))
+done
+
+sleep $time_wait
+killall -9 standard_read_samp
+sleep 1
+killall -9 dataset_read_samp
+#wait
+
